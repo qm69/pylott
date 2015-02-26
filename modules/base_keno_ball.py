fedb306475e25e6c2cf16fda4fa5fa9e5ce98070
qm69 <qm69@asd.qw>
2015-02-26 22:07:44 +0200

    create test_cases for base_keno_ball

--- modules/base_keno_ball.py
+++ modules/base_keno_ball.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+def ball_counter(draws):
+    """
+    Arguments:
+
+        draws @ list >> list >> int; like:
+        [...
+            [64,48,79,70,40,69,20,59,66,61,80,53,78,57,28,13,76,72,26,65],
+        ...]
+
+    Returns:
+
+        @ list >> dict;
+        [... {
+            'ball': 34,
+
+            'dropped': 1190,
+            'period': 3.97,
+
+            'missed': 1,
+            'silent': 12,
+
+            'max_pass': 28,
+            'max_inrow': 9,
+
+            series: [
+                [347, 123, 67, 45, 23, 12, 6, 3, 1],
+                [443, 117, 69, 51, 31, 15, 9, 6, 3, 1],
+            ]
+        }, ...]
+    """
+    response = []
+    for num in list(range(1, 81)):
+        for draw in draws:
+            pass
+
+    return []
--- tests/test_base_keno_ball.py
+++ tests/test_base_keno_ball.py
@@ -8,7 +8,7 @@ import pytest
 # http://stackoverflow.com/questions/714063/
 sys.path.append('/home/qm69/code/python/lottery')
 
-from pylott.modules.base_keno_ball import DrawCount
+from pylott.modules.base_keno_ball import ball_counter
 from pylott.results.keno_list_1000 import draw_list
 
 """
@@ -22,5 +22,47 @@ from pylott.results.keno_list_1000 import draw_list
 
 
 @pytest.fixture(scope="module")
-def dc():
-    return DrawCount(draw_list[:30], 5053)
+def bc():  # ball_counter == bc
+    return ball_counter(draw_list)
+
+
+class TestClass:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_ball_counter(self, bc):
+
+        # test the whole list
+        assert type(bc) is list
+        assert len(bc) == 80
+
+        # test each ball from list
+        for ball in bc:
+            assert type(ball) is dict
+
+            assert type(ball['ball']) is int
+            assert ball['ball'] < 81
+
+            assert type(ball['dropped']) is int
+            assert ball['dropped'] > 0
+
+            assert type(ball['period']) is float
+            assert ball['period'] < 5
+            assert ball['period'] > 3
+
+            assert type(ball['missed']) is int
+            assert ball['missed'] >= 0
+
+            assert type(ball['silent']) is int
+            assert ball['silent'] >= 0
+
+            assert type(ball['max_pass']) is int
+            assert ball['max_pass'] >= 0
+
+            assert type(ball['max_inrow']) is int
+            assert ball['max_inrow'] >= 0
+
+            assert type(ball['series']) is list
+            assert len(ball['series']) == 2
